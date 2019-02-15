9b5b16811c7c2d6b8ca95fb265da091a6afadb74
Jackie Pearce <jackiepearce83@hotmail.com>
2019-02-15 13:32:40 +0000

    Location and Grid classes made

---
+++ CodingGameMineSweeper/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGameMineSweeper
+{
+    public class Grid
+    {
+        public int NumberOfBombs { get; set; }
+
+        public List<Location> GridPattern { get; set; }
+
+        int BombsFound = 0;
+    }
+}
---
+++ CodingGameMineSweeper/Location.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGameMineSweeper
+{
+    public class Location
+    {
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public char Value { get; set; }
+
+        bool IsPossibleBomb()
+        {
+            if(Value == '?')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        string Numbers = "12345678";
+
+        bool IsANumber()
+        {
+            if (Numbers.Contains(Value))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsABomb { get; set; }
+    }
+}
